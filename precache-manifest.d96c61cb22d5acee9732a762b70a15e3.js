22cb726363cb0422a875b4e5a5cee63daf4f2204
MARIUS SWART <dswart54@yahoo.com>
2020-10-12 01:44:36 +0200

    Updates

---
+++ precache-manifest.d96c61cb22d5acee9732a762b70a15e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d6d3f465195f69d86da1e433fa13e9",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "0e5296dd9c0fe9cb9fcd",
+    "url": "/CV/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "88ab87ef4fcb3d67f136",
+    "url": "/CV/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0e5296dd9c0fe9cb9fcd",
+    "url": "/CV/static/js/2.38dee29f.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/CV/static/js/2.38dee29f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88ab87ef4fcb3d67f136",
+    "url": "/CV/static/js/main.276f0de6.chunk.js"
+  },
+  {
+    "revision": "39537d1727c9d7469390",
+    "url": "/CV/static/js/runtime-main.8c74ba61.js"
+  },
+  {
+    "revision": "4db4a34e198751f80d24cfd4fafdeca2",
+    "url": "/CV/static/media/Me.4db4a34e.jpeg"
+  }
+]);
